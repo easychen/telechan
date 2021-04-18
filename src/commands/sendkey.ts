1487b7fc8b83fc625b900da8a2a594836ae304da
EasyChen <easychen@qq.com>
2021-04-19 00:12:27 +0800

    修正域名:使用短域名,但不能带横线

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -13,9 +13,11 @@ const sendkey = () => (ctx: any) => {
   const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
   const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
 
+  const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
+
   replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
   🚀 Use follow url to send message : \n 
-  ${VERCEL_URL}/api/send?sendkey=<sendkey>&text=<text>`);
+  ${site_url}/api/send?sendkey=<sendkey>&text=<text>`);
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
 
